0238078754bc2153f56475839374979263843c08
raghav <1raghuram3@gmail.com>
2017-03-27 21:00:59 +0530

    closing the resource leak

--- spring4/src/main/java/com/bridgelabz/spring4/dao/Test.java
+++ spring4/src/main/java/com/bridgelabz/spring4/dao/Test.java
@@ -2,6 +2,7 @@ package com.bridgelabz.spring4.dao;
 
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.bridgelabz.spring4.model.Employee;
@@ -10,9 +11,11 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-       ApplicationContext context=new ClassPathXmlApplicationContext("applicatioonconfiguration");	
+		// if you are using the application context no need to create Beanfactory beacuse while  intializing it self it will create bean facatory
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicatioonconfiguration.xml");	
 		Employee employee=(Employee)context.getBean("employeebean");
 		employee.displayInfo();
-		
+		//closing the resource leak by downcasting the context object to ConConfigurableApplicationContext
+		((ConfigurableApplicationContext)context).close();
 	}
 }
