3ac2851877ffe789c4b0030f5b8ef55b37d1a1a1
raghava <1raghuram3@gmail.com>
2017-04-03 16:07:31 +0530

    student regiatration code without any validation

---
+++ studentreg1/src/main/java/com/bridgelabz/studentreg1/model/Student.java
@@ -0,0 +1,52 @@
+package com.bridgelabz.studentreg1.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="STUDENT REG")
+public class Student 
+{
+	@Id
+	@GenericGenerator(name="gen",strategy="increment")
+	@GeneratedValue(generator="gen")
+	@Column(name="id")
+	private int id;
+	@Column(name="student_name")
+    private String name;
+	@Column(name="email")
+	private String email;
+	@Column(name="password")
+	private String password;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
